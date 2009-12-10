be83c17e63ffca612662c8d1e732ce9f101c9017
Travis Reeder <travis@appoxy.com>
2009-12-09 21:03:21 -0800

    Scheduled tests.

---
+++ test/scheduled_runner.rb
@@ -0,0 +1,9 @@
+require 'simple_worker'
+
+class ScheduledRunner < SimpleWorker::Base
+
+    def scheduled_run(data=nil)
+        
+    end
+
+end
--- test/simple_worker_tests.rb
+++ test/simple_worker_tests.rb
@@ -9,6 +9,9 @@ class SimpleWorkerTests < Test::Unit::TestCase
         #puts @config.inspect
         @access_key = @config['simple_worker']['access_key']
         @secret_key = @config['simple_worker']['secret_key']
+
+        @worker = SimpleWorker::Service.new(@access_key, @secret_key)
+        @worker.host = "http://localhost:3000/api/"
     end
 
     def teardown
@@ -17,22 +20,21 @@ class SimpleWorkerTests < Test::Unit::TestCase
 
     def test_queue
 
-        worker = SimpleWorker::Service.new(@access_key, @secret_key, :host=>"http://localhost:3000/api/")
 
         # Upload latest runner code
-        worker.upload(File.join(File.dirname(__FILE__), "./test_runner.rb"), "TestRunner")
+        @worker.upload(File.join(File.dirname(__FILE__), "./test_runner.rb"), "TestRunner")
 
         # Add something to queue, get task ID back
         # Single task
-        response_hash = worker.queue("TestRunner", {"s3_key"=>"single runner", "times"=>10})
+        response_hash = @worker.queue("TestRunner", {"s3_key"=>"single runner", "times"=>10})
         # task set
-        response_hash = worker.queue("TestRunner", [{"id"=>"local_id", "s3_key"=>"some key", "times"=>4}, {"s3_key"=>"some key2", "times"=>3}, {"s3_key"=>"some key", "times"=>2}])
+        response_hash = @worker.queue("TestRunner", [{"id"=>"local_id", "s3_key"=>"some key", "times"=>4}, {"s3_key"=>"some key2", "times"=>3}, {"s3_key"=>"some key", "times"=>2}])
 
         # Check status
         tasks = response_hash["tasks"]
         while tasks.size > 0
             tasks.each do |t|
-                status_response = worker.status(t["task_id"])
+                status_response = @worker.status(t["task_id"])
                 puts 'status for ' + t["task_id"] + ' = ' + status_response["status"]
                 if status_response["status"] == "complete"
                     tasks.delete(t)
@@ -40,6 +42,29 @@ class SimpleWorkerTests < Test::Unit::TestCase
             end
         end
 
+
+    end
+
+    def test_scheduled
+
+# Upload latest runner code
+        @worker.upload(File.join(File.dirname(__FILE__), "./test_runner.rb"), "TestRunner")
+
+# Add something to queue, get task ID back
+        start_at = 10.seconds.since
+#start_at = start_at.gmtime # testing different timezone
+        puts 'start_at =' + start_at.inspect
+        response_hash = @worker.schedule("TestRunner", {"msg"=>"One time test."}, {:start_at=>start_at})
+        puts 'response_hash=' + response_hash.inspect
+
+        start_at = 10.seconds.since
+        response_hash = @worker.schedule("TestRunner", {"msg"=>"Run times test"}, {:start_at=>start_at, :run_every=>30, :run_times=>3})
+        puts 'response_hash=' + response_hash.inspect
+
+        start_at = 10.seconds.since
+        end_at = 2.minutes.since
+        response_hash = @worker.schedule("TestRunner", {"msg"=>"End at test"}, {:start_at=>start_at, :run_every=>30, :end_at=>end_at, :run_times=>20})
+        puts 'response_hash=' + response_hash.inspect
 
     end
 end
