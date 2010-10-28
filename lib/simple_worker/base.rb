d1fa6858638aa6c99db38ed4ddb362e4d659ff83
Travis Reeder <travis@appoxy.com>
2010-10-28 00:02:27 -0700

    Added some docs.

--- lib/simple_worker/base.rb
+++ lib/simple_worker/base.rb
@@ -95,6 +95,16 @@ module SimpleWorker
             upload_if_needed
         end
 
+        #
+        # schedule: hash of scheduling options that can include:
+        #     Required:
+        #     - start_at:      Time of first run - DateTime or Time object.
+        #     Optional:
+        #     - run_every:     Time in seconds between runs. If ommitted, task will only run once.
+        #     - delay_type:    Fixed Rate or Fixed Delay. Default is fixed_delay.
+        #     - end_at:        Scheduled task will stop running after this date (optional, if ommitted, runs forever or until cancelled)
+        #     - run_times:     Task will run exactly :run_times. For instance if :run_times is 5, then the task will run 5 times.
+        #
         def schedule(schedule)
             upload_if_needed
 
