4147f0481c3a5faae1beea6567d36ca15ebe86cb
Travis Reeder <travis@appoxy.com>
2011-09-28 15:12:24 -0700

    replaced access stuff with token.

--- lib/simple_worker/server/runner.rb
+++ lib/simple_worker/server/runner.rb
@@ -17,9 +17,9 @@ def init_mailer(sw_config)
     mailer_config = sw_config['mailer']
     if mailer_config
       require 'action_mailer'
-       ActionMailer::Base.raise_delivery_errors = true
-       ActionMailer::Base.smtp_settings =  mailer_config
-       ActionMailer::Base.delivery_method = :smtp
+      ActionMailer::Base.raise_delivery_errors = true
+      ActionMailer::Base.smtp_settings = mailer_config
+      ActionMailer::Base.delivery_method = :smtp
     end
   end
 end
@@ -61,10 +61,11 @@ end
 def init_worker_service_for_runner(job_data)
   SimpleWorker.configure do |config|
     sw_config = job_data['sw_config']
-    config.access_key = sw_config['access_key']
-    config.secret_key = sw_config['secret_key']
+    config.token = sw_config['token']
+    config.project_id = sw_config['project_id']
     #puts 'Setting host to ' + host.inspect
     config.host = sw_config['host'] if sw_config['host']
+    config.host = sw_config['port'] if sw_config['port']
     db_config = sw_config['database']
     if db_config
       config.database = db_config
@@ -77,7 +78,7 @@ def init_worker_service_for_runner(job_data)
   end
 end
 
-
+# SimpleWorker.logger.level ==  Logger::DEBUG
 run_data = JSON.load(File.open(run_data_file))
 # Load in job data
 job_data = JSON.load(File.open(job_data_file))
@@ -85,14 +86,14 @@ job_data.merge!(run_data)
 SimpleWorker.logger.debug 'job_data=' + job_data.inspect
 
 sw_config = job_data['sw_config']
-  init_database_connection(sw_config)
-  init_mailer(sw_config)
-  SimpleWorker.disable_queueing()
-  runner_class = get_class_to_run(job_data['class_name'])
-  SimpleWorker.running_class = runner_class
-  runner = init_runner(runner_class, job_data, dirname)
-  init_worker_service_for_runner(job_data)
-  SimpleWorker.enable_queueing()
+init_database_connection(sw_config)
+init_mailer(sw_config)
+SimpleWorker.disable_queueing()
+runner_class = get_class_to_run(job_data['class_name'])
+SimpleWorker.running_class = runner_class
+runner = init_runner(runner_class, job_data, dirname)
+init_worker_service_for_runner(job_data)
+SimpleWorker.enable_queueing()
 
 # Let's run it!
-  runner_return_data = runner.run
+runner_return_data = runner.run
