0e9fa5faf375b2c905b29d20e4902da8fedd1405
Travis Reeder <travis@appoxy.com>
2011-11-09 18:32:41 -0800

    Testing crazy gem merge action.

--- test/quick_run.rb
+++ test/quick_run.rb
@@ -7,19 +7,21 @@ require_relative 'merging_worker'
 class QuickRun < TestBase
 
   def test_worker
-    worker = BrokenLoadWorker.new
-    worker.queue
+    1.times do |i|
+      worker = BrokenLoadWorker.new
+      worker.queue
 
-    status = worker.wait_until_complete
-    p status
-    p status["error_class"]
-    p status["msg"]
-    puts "\n\n\nLOG START:"
-    log = worker.get_log
-    puts log
-    puts "LOG END\n\n\n"
-    assert status["status"] == "complete", "Status was not complete, it was #{status["status"]}"
-    assert log.include?(worker.s3_key)
+      status = worker.wait_until_complete
+      p status
+      p status["error_class"]
+      p status["msg"]
+      puts "\n\n\nLOG START:"
+      log = worker.get_log
+      puts log
+      puts "LOG END\n\n\n"
+      assert status["status"] == "complete", "Status was not complete, it was #{status["status"]}"
+      assert log.include?("hello")
+    end
   end
 
 end
