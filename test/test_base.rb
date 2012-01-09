2b123ca913bf611682496974620bd423c382731d
Travis Reeder <treeder@gmail.com>
2012-01-09 12:27:20 -0800

    Turned off beta by default.

--- test/test_base.rb
+++ test/test_base.rb
@@ -35,7 +35,7 @@ class TestBase < Test::Unit::TestCase
     @project_id = @config['iron_worker']['project_id']
     # new style
     IronWorker.configure do |config|
-      config.beta=true
+      config.beta=false
       config.token = @token
       config.project_id = @project_id
       config.host = @config['iron_worker']['host'] if @config['iron_worker']['host']
