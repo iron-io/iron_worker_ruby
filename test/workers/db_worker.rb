a2b7239b042ec1e6633c53adab50f5620357a7fe
Travis Reeder <treeder@gmail.com>
2012-05-26 22:38:57 -0700

    Removed mysql dependency.

--- test/workers/db_worker.rb
+++ test/workers/db_worker.rb
@@ -1,27 +1,27 @@
 # bump..........
-
-require 'mysql2'
-
-class DbWorker < IronWorker::Base
-  merge_gem 'activerecord', :require=>'active_record'
-  merge '../models/db_model'
-
-
-  def run
-
-    n = DbModel.new(:name=>"jane", :age=>21)
-    n.save!
-
-    to = DbModel.first
-    puts 'found: ' + to.inspect
-    @object = to
-
-    n.delete
-  end
-
-  def ob
-    @object
-  end
-
-
-end
+#
+#require 'mysql2'
+#
+#class DbWorker < IronWorker::Base
+#  merge_gem 'activerecord', :require=>'active_record'
+#  merge '../models/db_model'
+#
+#
+#  def run
+#
+#    n = DbModel.new(:name=>"jane", :age=>21)
+#    n.save!
+#
+#    to = DbModel.first
+#    puts 'found: ' + to.inspect
+#    @object = to
+#
+#    n.delete
+#  end
+#
+#  def ob
+#    @object
+#  end
+#
+#
+#end
