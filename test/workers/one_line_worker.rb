9ca9527dc537686dff7a72241380a5bcca69cb46
Roman <rkononov@gmail.com>
2012-07-15 15:37:43 +0600

    removed rest-client from one-line worker

--- test/workers/one_line_worker.rb
+++ test/workers/one_line_worker.rb
@@ -1,17 +1,10 @@
 # bump......
 class OneLineWorker < IronWorker::Base
 
-  merge_gem 'iron_mq'
-  merge_gem 'rest-client', :require=>'rest_client'
 
   attr_accessor :x
 
   def run
     puts "hello world! #{x}"
-    mq = IronMQ::Client
-
-    puts 'getting url'
-    r = RestClient.get 'http://example.com/resource'
-    p r.body
   end
 end
